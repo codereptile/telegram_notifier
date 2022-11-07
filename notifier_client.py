af4417b0a8d23d7aafa19374e2e78074631b194c
time_skipper <sasha.shvech2003@mail.ru>
2022-11-07 11:06:34 +0300

    multiprocess client try

--- notifier_client.py
+++ notifier_client.py
@@ -3,16 +3,20 @@ import re
 import threading
 
 import subprocess
+import time
+from multiprocessing import Process
 
 new_messages = []
 
-DEFAULT_DISK_USAGE_PERCENT_TRIGGER = 1
+DEFAULT_DISK_USAGE_PERCENT_TRIGGER = 70
 disk_usage_percent_trigger = DEFAULT_DISK_USAGE_PERCENT_TRIGGER
 
 CHECK_SERVER_INTERVAL = 3
 SEND_UPDATES_INTERVAL = 1
 
 messages_mutex = threading.Lock()
+
+FILE_SYSTEM_NAME = "/dev/nvme0n1p2.*"
 
 
 async def check_server_status():
@@ -21,7 +25,7 @@ async def check_server_status():
     while True:
         output_message = ""
         output_string = subprocess.run(["df", "-h"], capture_output=True).stdout.decode("utf-8")
-        match = re.search("/dev/nvme0n1p5.*", output_string)
+        match = re.search(FILE_SYSTEM_NAME, output_string)
         if match is None:
             return "SERVER STATUS CHECK FAILED! (df -h)"
         match = re.search("[0-9]*%", match[0])
@@ -35,12 +39,13 @@ async def check_server_status():
             disk_usage_percent_trigger = DEFAULT_DISK_USAGE_PERCENT_TRIGGER
 
         if output_message:
+            messages_mutex.acquire()
             new_messages.append(output_message)
+            messages_mutex.release()
+        time.sleep(CHECK_SERVER_INTERVAL)
 
-        await asyncio.sleep(CHECK_SERVER_INTERVAL)
 
-
-async def send_updates():
+def send_updates():
     global new_messages
 
     while True:
@@ -51,14 +56,44 @@ async def send_updates():
 
         new_messages = []
         messages_mutex.release()
-        await asyncio.sleep(SEND_UPDATES_INTERVAL)
+        print(2)
+        time.sleep(SEND_UPDATES_INTERVAL)
 
 
-async def main():
-    sending_updates = asyncio.create_task(send_updates())
-    checking_server_status = asyncio.create_task(check_server_status())
-    while True:
-        await checking_server_status
-        await sending_updates
+class Task:
+    process = Process
 
-asyncio.run(main())
+    def __init__(self, function):
+        self.function = function
+
+    def start(self):
+        process = Process(target=self.function)
+        process.start()
+
+    def get_process(self):
+        return self.process
+
+    def end(self):
+        self.process.close()
+
+
+if __name__ == '__main__':
+    print("Client starts")
+
+    tasks = list()
+
+    task1 = Task(check_server_status())
+    task2 = Task(send_updates())
+    task1.start()
+    task2.start()
+    tasks.append(task1)
+    tasks.append(task2)
+    for task in tasks:
+        task.get_process().join()
+
+    # wait_until_stop_signal()
+
+    # GRACEFUL STOP
+    for task in tasks:
+        task.stop()
+    print("Client stops now")
