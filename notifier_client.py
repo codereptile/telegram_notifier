e1a0b68b42e95bdc6efac5bbd0080ebaf1ecf8d6
Artem Ageev <codereptile@yandex.ru>
2022-11-07 12:40:43 +0300

    switch to threads, refactor and update code

--- notifier_client.py
+++ notifier_client.py
@@ -1,98 +1,87 @@
-import asyncio
 import re
+import signal
 import threading
-
 import subprocess
 import time
-from multiprocessing import Process
 
 new_messages = []
+stop_all_threads = False
 
-DEFAULT_DISK_USAGE_PERCENT_TRIGGER = 70
+DEFAULT_DISK_USAGE_PERCENT_TRIGGER = 1
 disk_usage_percent_trigger = DEFAULT_DISK_USAGE_PERCENT_TRIGGER
-
-CHECK_SERVER_INTERVAL = 3
-SEND_UPDATES_INTERVAL = 1
 
 messages_mutex = threading.Lock()
 
-FILE_SYSTEM_NAME = "/dev/nvme0n1p5.*"
+FILE_SYSTEM_NAME = "/dev/nvme0n1p2.*"
 
 
 def check_server_status():
     global disk_usage_percent_trigger
     global new_messages
-    while True:
-        output_message = ""
-        output_string = subprocess.run(["df", "-h"], capture_output=True).stdout.decode("utf-8")
-        match = re.search(FILE_SYSTEM_NAME, output_string)
-        if match is None:
-            return "SERVER STATUS CHECK FAILED! (df -h)"
-        match = re.search("[0-9]*%", match[0])
-        if match is None:
-            return "SERVER STATUS CHECK FAILED! (df -h)"
-        disk_usage_percentage = int(match[0][:-1])
-        if disk_usage_percentage >= disk_usage_percent_trigger:
-            disk_usage_percent_trigger = disk_usage_percentage + 1
-            output_message += "Warning: disk usage reached " + str(disk_usage_percentage) + "%\n"
-        elif disk_usage_percentage < DEFAULT_DISK_USAGE_PERCENT_TRIGGER:
-            disk_usage_percent_trigger = DEFAULT_DISK_USAGE_PERCENT_TRIGGER
 
-        if output_message:
-            messages_mutex.acquire()
-            new_messages.append(output_message)
-            messages_mutex.release()
-        time.sleep(CHECK_SERVER_INTERVAL)
+    output_message = ""
+    output_string = subprocess.run(["df", "-h"], capture_output=True).stdout.decode("utf-8")
+    match = re.search(FILE_SYSTEM_NAME, output_string)
+    if match is None:
+        return "SERVER STATUS CHECK FAILED! (df -h)"
+    match = re.search("[0-9]*%", match[0])
+    if match is None:
+        return "SERVER STATUS CHECK FAILED! (df -h)"
+    disk_usage_percentage = int(match[0][:-1])
+    if disk_usage_percentage >= disk_usage_percent_trigger:
+        disk_usage_percent_trigger = disk_usage_percentage + 1
+        output_message += "Warning: disk usage reached " + str(disk_usage_percentage) + "%\n"
+    elif disk_usage_percentage < DEFAULT_DISK_USAGE_PERCENT_TRIGGER:
+        disk_usage_percent_trigger = DEFAULT_DISK_USAGE_PERCENT_TRIGGER
+
+    if output_message:
+        messages_mutex.acquire()
+        new_messages.append(output_message)
+        messages_mutex.release()
 
 
 def send_updates():
     global new_messages
 
-    while True:
-        messages_mutex.acquire()
-        if len(new_messages) != 0:
-            for message in new_messages:
-                print(message)
+    messages_mutex.acquire()
+    if len(new_messages) != 0:
+        for message in new_messages:
+            print(message)
 
-        new_messages = []
-        messages_mutex.release()
-        time.sleep(SEND_UPDATES_INTERVAL)
+    new_messages = []
+    messages_mutex.release()
 
 
-class Task:
-    process = Process
+class Task(threading.Thread):
+    def __init__(self, target, sleep):
+        def periodic_caller():
+            while not stop_all_threads:
+                target()
+                time.sleep(sleep)
 
-    def __init__(self, function):
-        self.function = function
+        super().__init__(target=periodic_caller)
 
-    def start(self):
-        process = Process(target=self.function)
-        process.start()
 
-    def get_process(self):
-        return self.process
+def graceful_stop(*args):
+    global stop_all_threads
 
-    def end(self):
-        self.process.close()
+    print("Client stop initialized")
+    stop_all_threads = True
 
 
 if __name__ == '__main__':
     print("Client starts")
 
-    tasks = list()
-
-    task1 = Task(check_server_status())
-    task2 = Task(send_updates())
-    task1.start()
-    task2.start()
-    tasks.append(task1)
-    tasks.append(task2)
+    tasks = [
+        Task(check_server_status, 1),
+        Task(send_updates, 1)
+    ]
     for task in tasks:
-        task.get_process().join()
+        task.start()
 
-    # wait_until_stop_signal()
+    signal.signal(signal.SIGINT, graceful_stop)
+    signal.signal(signal.SIGTERM, graceful_stop)
 
-    # GRACEFUL STOP
     for task in tasks:
-        task.stop()
-    print("Client stops now")
+        task.join()
+    print("Client stop finished")
